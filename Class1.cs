d4a4b5de31d3022e3dd35b0335faf9c1cb984852
YuliaDe <31962554+YuliaDe@users.noreply.github.com>
2019-08-15 02:34:19 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+
+namespace Calculator
+{
+    public class Go
+    {
+   
+        public static string result;
+        public static string VScobcah;
+     
+        public static void Information()
+        {
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("\t\tКОНСОЛЬНЫЙ КАЛЬКУЛЯТОР");
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("Операции: сложение, вычитание, умножение и деление.");
+            Console.WriteLine("Приоритеты можно изменять с помощью скобок.");
+            Console.WriteLine("Дробная часть отделяется запятой.");
+            Console.WriteLine("Введите, например, 80*(6-2)-500/5 и нажмите Enter");
+            Console.WriteLine("Для выхода из калькулятора нажмите 'н'");
+            Console.WriteLine("**********************************************************");
+        }
+
+        public static bool NotOperator(int j)
+        {
+            return VScobcah[j] != '+' && VScobcah[j] != '-' && VScobcah[j] != '*' && VScobcah[j] != '/';
+        }
+
+        public static double GetLeftOperand(int i)
+        {
+            
+                string LeftOperand = "";
+                for (int j = i - 1; j >= 0; j--)
+                    if (NotOperator(j))
+                        LeftOperand = VScobcah[j] + LeftOperand;
+                    else
+                        break;
+                return double.Parse(LeftOperand);
+          
+        }
+
+        public static double GetRightOperand(int i)
+        {
+            string RightOperand = "";
+            for (int j = i + 1; j < VScobcah.Length; j++)
+                if (NotOperator(j))
+                    RightOperand += VScobcah[j];
+                else
+                    break;
+            return double.Parse(RightOperand);
+        }
+
+        public static void ReplaceExp(int i, double ToThis)
+        {
+            int FromI = 0;
+            int ToI = VScobcah.Length - 1;
+            for (int j = i - 1; j >= 0; j--)
+                if (NotOperator(j))
+                    FromI = j;
+                else
+                    break;
+            for (int j = i + 1; j < VScobcah.Length; j++)
+                if (NotOperator(j))
+                    ToI = j;
+                else
+                    break;
+            VScobcah = VScobcah.Replace(VScobcah.Substring(FromI, ToI - FromI + 1), ToThis.ToString());
+        }
+
+        public static void vibUmnDel(int i)
+        {
+            double UmnogDelen;
+            if (VScobcah[i] == '*')
+                UmnogDelen = GetLeftOperand(i) * GetRightOperand(i);
+            else
+                UmnogDelen = GetLeftOperand(i) / GetRightOperand(i);
+            ReplaceExp(i, UmnogDelen);
+            Calcul();
+        }
+
+        public static void vibPlMin(int i)
+        {
+            double PlMin;
+            if (VScobcah[i] == '+')
+                PlMin = GetLeftOperand(i) + GetRightOperand(i);
+            else
+                PlMin = GetLeftOperand(i) - GetRightOperand(i);
+            ReplaceExp(i, PlMin);
+            Calcul();
+        }
+
+        public static void Calcul()
+        {
+            int i;
+            for (i = 0; i < VScobcah.Length; i++)
+                if (VScobcah[i] == '*' || VScobcah[i] == '/')
+                {
+                    vibUmnDel(i);
+                    return;
+                }
+            for (i = 0; i < VScobcah.Length; i++)
+                if (VScobcah[i] == '+' || VScobcah[i] == '-')
+                {
+                    vibPlMin(i);
+                    return;
+                }
+        }
+        public static bool FindBrackets(out int o)
+        {
+            o = 0;
+            if (result.IndexOf('(') != -1)
+            {
+                int ClosedBracket = result.IndexOf(')');
+                int OpenBracket = 0;
+                for (int i = ClosedBracket - 1; i >= 0; i--)
+                    if (result[i] == '(')
+                    {
+                        OpenBracket = i;
+                        VScobcah = result.Substring(OpenBracket + 1, ClosedBracket - OpenBracket - 1);
+                        o = OpenBracket;
+                        result = result.Remove(OpenBracket, ClosedBracket - OpenBracket + 1);
+                        break;
+                    }
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using static Calculator.Go;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    class PerformOperation
+    {
+
+        static void Main(string[] args)
+        {
+            char l = 'д';
+            while (l == 'д')
+            {
+                Information();
+                Console.Write("Введите выражение: ");
+             
+
+                string pattern = @"[0-9]|\[([^\[\]]+)\]";//для дальнейшей проверки на буквы или любые недопустимые символы
+               
+                result = '(' + Console.ReadLine().Replace(" ", "") + ')';
+ if (Regex.IsMatch(result, pattern, RegexOptions.IgnoreCase)) //проверка
+                {
+                    int o;
+                    while (FindBrackets(out o))
+                    {
+                        Calcul();
+                        result = result.Insert(o, VScobcah);
+                    }
+                    Console.WriteLine("Ответ: " + result);
+                }
+                else
+               { Console.WriteLine("Это не число!"); }
+
+                Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)");
+                l = Convert.ToChar(Console.ReadLine());
+            }
+            
+            Console.Out.Close();
+        }
+    }
+}
+
